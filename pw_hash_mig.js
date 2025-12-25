80bd27341410ed1e0bdfb7962b93796a86b7161d
gayathri-0902 <gayathrisettipalli2@gmail.com>
2025-12-25 21:14:00 +0530

    password hashing

--- pw_hash_mig.js
+++ pw_hash_mig.js
@@ -1,19 +1,22 @@
 const bcrypt = require("bcrypt");
 const { MongoClient } = require("mongodb");
+const dotenv = require("dotenv");
+dotenv.config();
+const MONGO_URI = process.env.MONGO_URI;
 
-const uri = "mongodb://localhost:27017";
+const uri = MONGO_URI;
 const client = new MongoClient(uri);
 const SALT_ROUNDS = 10;
 
 async function migratePasswords() {
   await client.connect();
-  const db = client.db("your_db_name");
+  const db = client.db("attend_sys");
 
   const users = await db.collection("users").find({}).toArray();
 
   for (const user of users) {
     // Skip already hashed passwords
-    if (user.password.startsWith("$2b$")) continue;
+    // if (user.password.startsWith("$2b$")) continue;
 
     const hashed = await bcrypt.hash(user.password, SALT_ROUNDS);
 
