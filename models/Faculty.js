34b6e51fe4741fc461637efce79ae11015099cd2
gayathri-0902 <gayathrisettipalli2@gmail.com>
2026-04-16 01:14:31 +0530

    updated models for is_archived field

--- models/Faculty.js
+++ models/Faculty.js
@@ -10,6 +10,7 @@ const FacultySchema = new mongoose.Schema(
     },
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
+    is_archived: { type: Boolean, default: false },
   },
   { collection: "faculties" }
 );
